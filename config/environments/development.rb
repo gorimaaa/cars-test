134f2888db750d2d712e5cecd84af6656a968012
gorimaaa <juba.belhocine@outlook.fr>
2025-03-20 16:10:15 +0100

    Add initial tests, images, and Tailwind CSS integration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,6 +37,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Don't care if the mailer can't send.
@@ -70,4 +71,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  # 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,21 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   get "dashboard/send_reminder"
   get "dashboard/index"
+  get "reservations/user_reservations"
+  get "reservations/pay_reservation"
+  get "reservations/pay_confirmation"
+  get 'reservations/pay_reservation/:id_book', to: 'reservations#pay_reservation', as: 'pay_reservation'
+  post 'reservations/user_payed_book', to: 'reservations#user_payed_book', as: 'user_payed_book'
+  post 'reservations/:id/book', to: 'reservations#book', as: 'book_reservation'
+  post 'reservations/:id/cancel_book', to: 'reservations#cancel_book', as: 'cancel_book_reservation'
+  post 'dashboard/confirmation_mail/:reservation_id', to: 'dashboard#send_confirmation_admin', as: 'send_confirmation'
+  post 'dashboard/reminder_mail/:reservation_id', to: 'dashboard#send_reminder_admin', as: 'send_reminder_admin'
+
+
+  root "dashboard#index"
 end
