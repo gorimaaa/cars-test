8ea36e398a936a0ff7173e58a1f043120b68854e
gorimaaa <juba.belhocine@outlook.fr>
2025-03-20 16:11:12 +0100

    Add email functionalities for reservation confirmations and reminders

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,9 +1,29 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "bot@roadstr.fr"
   layout "mailer"
 
-  def send_reminder(booking)
-    details = {to: 'fakeuser@roadstr.fr',
+  def send_confirmation(reservation, user)
+    @user = user
+    @reservation = reservation
+    details = {from: "admin@roadstr.fr",
+               to: user.email,
+               object: 'Confirmation for your booking'}
+    mail(details)
+  end
+
+  def send_reminder_admin(reservation, user)
+    @user = user
+    @reservation = reservation
+    details = {from: "admin@roadstr.fr",
+               to: user.email,
+               object: 'Reminder for your booking from admin'}
+    mail(details)
+  end
+  
+  def send_reminder(reservation, user)
+    @user = user
+    @reservation = reservation
+    details = {to: user.email,
                object: 'Reminder for your booking'}
     mail(details)
   end
